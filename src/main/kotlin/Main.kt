53eaf5670238f08bc7d59413e5d5a6c834acd584
BirnDo <40146720160258@litec.ac.at>
2021-11-28 13:45:36 +0100

    closes #1
    Created project structure and set up first Test

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+fun main() {
+    //Test before Day 1
+    print("Pre-Advent Test")
+
+    //@ToDo Day 1
+
+    //@ToDo Day 2
+
+    //@ToDo Day 3
+
+    //@ToDo Day 4
+
+    //@ToDo Day 5
+
+    //@ToDo Day 6
+
+    //@ToDo Day 7
+
+    //@ToDo Day 8
+
+    //@ToDo Day 9
+
+    //@ToDo Day 10
+
+    //@ToDo Day 11
+
+    //@ToDo Day 12
+
+    //@ToDo Day 13
+
+    //@ToDo Day 14
+
+    //@ToDo Day 15
+
+    //@ToDo Day 16
+
+    //@ToDo Day 17
+
+    //@ToDo Day 18
+
+    //@ToDo Day 19
+
+    //@ToDo Day 20
+
+    //@ToDo Day 21
+
+    //@ToDo Day 22
+
+    //@ToDo Day 23
+
+    //@ToDo Day 24
+
+    //@ToDo Day 25
+}
